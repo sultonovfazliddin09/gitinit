eb274d534a38b615090bf2fec98ff37a1e8d12c2
mahmudjooo <fusmonova818@gmail.com>
2025-07-08 15:12:43 +0300

    contact qoshildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/shared/navbar'
 import Hero from './components/hero/hero'
+import Contact from './components/contact/contact'
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Contact/>
     </div>
   )
 }
